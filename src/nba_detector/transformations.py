c47d5818a94322131b637eb859488b141629ff04
Francisco Zenteno <zenteno.fco@gmail.com>
2023-04-25 16:56:47 -0700

    restructuring project 2 and added example transformation

---
+++ src/nba_detector/transformations.py
@@ -0,0 +1,20 @@
+# The purpose of this module is to provide an interface to select different types of transformations
+# Examples: https://albumentations.ai/docs/getting_started/bounding_boxes_augmentation/
+
+import albumentations as A
+import cv2
+
+def get_transform(format:str = "coco") -> A.Compose:
+    """
+    Retrieves an example of a transformation
+    """
+    if format not in ["pascal_voc", "albumentations", "coco", "yolo"]:
+        raise ValueError(f"Format not recognized, it was sent {format}")
+    
+    transform = A.Compose([
+        A.HorizontalFlip(p=0.5),
+        A.RandomBrightnessContrast(p=0.2),
+        ], 
+        bbox_params=A.BboxParams(format = format))
+    return transform
+
