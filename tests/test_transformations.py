5b143e710afdc9485db761717e948c4fbdd97d66
Francisco Zenteno <zenteno.fco@gmail.com>
2023-04-25 17:38:19 -0700

    added another simple test

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,3 +1,4 @@
+import albumentations as A
 from nba_detector.transformations import get_transformation
 import pytest
 
@@ -6,3 +7,7 @@ def test_get_transformation_bad_format():
     for bad_input in bad_input_examples:
         with pytest.raises(ValueError):
             get_transformation(bad_input)
+
+def test_get_transformation_return_type():
+    output = get_transformation()
+    assert(type(output) == A.Compose)
