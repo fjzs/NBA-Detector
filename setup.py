4180f0a6de0e175d634d299158c59220365869e1
Francisco Zenteno <zenteno.fco@gmail.com>
2023-05-01 20:54:25 -0700

    testing if setup and toml files are needed

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-# this is going to allow us to install the package in editable mode.
-# if we change the src/nba_detector package we don't need to re-install the package
-if __name__ == "__main__":
-    #setup()
-    print()
