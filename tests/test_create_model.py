c8ff583c61336695b554ce6950bee578fd4740f8
Francisco Zenteno <zenteno.fco@gmail.com>
2023-05-01 19:59:26 -0700

    added some tests

--- tests/test_create_model.py
+++ tests/test_create_model.py
@@ -3,7 +3,7 @@ import torch
 import pytest
 
 
-""" def test_get_model_good_input():
+def test_get_model_good_input():
     model_names = ["fasterrcnn"]
     num_classes = [2]
     trainable_layers = [1]
@@ -12,10 +12,24 @@ import pytest
             for t in trainable_layers:
                 model = get_model(m,n,t)
                 assert (type(model) == torch.nn.Module)
- """
 
-def test_get_model_bad_format():
+
+def test_get_model_bad_name():
     bad_model_name = "Fasterrcnn"
     with pytest.raises(ValueError):
         get_model(bad_model_name, 2)
- 
+
+def test_get_model_bad_num_classes():
+    good_model_name = "fasterrcnn"
+    bad_num_classes = [None, -1, 0, 1]
+    for x in bad_num_classes:
+        with pytest.raises(ValueError):
+            get_model(good_model_name, x)
+
+def test_get_model_bad_trainable_layers():
+    good_model_name = "fasterrcnn"
+    good_num_classes = 2
+    bad_trainable_num = [-1,6]
+    for x in bad_trainable_num:
+        with pytest.raises(ValueError):
+            get_model(good_model_name, good_num_classes, x)
