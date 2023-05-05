617466fc28f843014af3211208223f688eef4230
Khushi-Purohit <unayshah@gmail.com>
2023-05-04 17:51:46 -0700

    Merged req from main

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -3,9 +3,11 @@ from src.nba_detector.transformations import get_transformation
 from src.nba_detector.dataloader import load_data
 import pytest
 
-def test_get_dataset():
-    trainloader, valloader, testloader = load_data('/content/NBA-Player-Detector-1/', 224, 224, 4)
 
-    assert(trainloader != None)
-    assert(testloader != None)
-    assert(valloader != None)
+def test_get_dataset():
+    trainloader, valloader, testloader = load_data(
+        '/content/NBA-Player-Detector-1/', 224, 224, 4)
+
+    assert (trainloader != None)
+    assert (testloader != None)
+    assert (valloader != None)
