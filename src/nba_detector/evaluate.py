11e4fb9e27299387e9967ba467f45f824cab81e9
HarshSulakhe <hsulakhe@ucsd.edu>
2023-05-16 00:44:51 -0700

    Fixed typos, added test code

--- src/nba_detector/evaluate.py
+++ src/nba_detector/evaluate.py
@@ -2,7 +2,7 @@ import torch
 from torchmetrics.detection.mean_ap import MeanAveragePrecision
 
 
-def update_metric(metric:MeanAveragePrecision(), preds:list(dict), target:list(dict)) -> dict:
+def update_metric(metric:MeanAveragePrecision(), preds:list, targets:list) -> dict:
     """
     metric: An instance of MeanAveragePrecision()
     preds (List): 
@@ -14,7 +14,7 @@ def update_metric(metric:MeanAveragePrecision(), preds:list(dict), target:list(d
     labels: IntTensor of shape (num_boxes) containing 0-indexed detection classes for the boxes.
     masks: bool of shape (num_boxes, image_height, image_width) containing boolean masks. Only required when iou_type=”segm”.
 
-    target (List): A list consisting of dictionaries each containing the key-values (each dictionary corresponds to a single image). 
+    targets (List): A list consisting of dictionaries each containing the key-values (each dictionary corresponds to a single image). 
     Parameters that should be provided per dict:
         boxes: FloatTensor of shape (num_boxes, 4) containing num_boxes ground truth boxes of the format specified in the constructor. 
             By default, this method expects (xmin, ymin, xmax, ymax) in absolute image coordinates.
@@ -27,7 +27,7 @@ def update_metric(metric:MeanAveragePrecision(), preds:list(dict), target:list(d
     metric.update(preds,targets)
     return metric
 
-def update_metric_on_batch(metric:MeanAveragePrecision(), model:torch.nn.Module, image_batch:torch.Tensor, targets:list(dict)) -> dict:
+def update_metric_on_batch(metric:MeanAveragePrecision(), model:torch.nn.Module, image_batch:torch.Tensor, targets:list) -> dict:
     """
     model: A PyTorch model of type torch.nn.Module returned from create_model.py
     image_batch: A batch of images of type torch.Tensor
@@ -44,10 +44,10 @@ def update_metric_on_batch(metric:MeanAveragePrecision(), model:torch.nn.Module,
     model.eval()
     with torch.no_grad():
         preds = model(image_batch)
-        metric = update_metric(metric,preds,targets)
+        update_metric(metric,preds,targets)
     return metric
 
-def update_metric_on_dataloader(metric:MeanAveragePrecision(), model:torch.nn.Module, dataloader:torch.data.utils.Dataloader) -> dict:
+def update_metric_on_dataloader(metric:MeanAveragePrecision(), model:torch.nn.Module, dataloader:torch.utils.data.DataLoader) -> dict:
     """
     metric: An instance of MeanAveragePrecision()
     model: A PyTorch model of type torch.nn.Module returned from create_model.py
@@ -59,11 +59,11 @@ def update_metric_on_dataloader(metric:MeanAveragePrecision(), model:torch.nn.Mo
     model.eval()
     with torch.no_grad():
         for i, (image_batch,targets) in enumerate(dataloader):
-            metric = update_metric_on_batch(metric,model,image_batch,targets)
+            update_metric_on_batch(metric,model,image_batch,targets)
 
     return metric
     
-def evaluate_dataloader(model:torch.nn.Module, dataloader:torch.data.utils.Dataloader) -> dict : 
+def evaluate_dataloader(model:torch.nn.Module, dataloader:torch.utils.data.DataLoader) -> dict : 
     """
     model: A PyTorch model of type torch.nn.Module returned from create_model.py
     dataloader: An instance of torch.data.utils.Dataloader
@@ -86,11 +86,11 @@ def evaluate_dataloader(model:torch.nn.Module, dataloader:torch.data.utils.Datal
         mar_100_per_class: (Tensor) (-1 if class metrics are disabled)
     """
     metric = MeanAveragePrecision(iou_type = 'bbox', class_metrics = True)
-    metric = update_metric_on_dataloader(metric,model,dataloader)
+    update_metric_on_dataloader(metric,model,dataloader)
     mAP_dict = metric.compute()
     return mAP_dict
 
-def evaluate_batch(model:torch.nn.Module, image_batch:torch.Tensor, targets:list(dict)) -> dict :
+def evaluate_batch(model:torch.nn.Module, image_batch:torch.Tensor, targets:list) -> dict :
     """
     model: A PyTorch model of type torch.nn.Module returned from create_model.py
     image_batch: A batch of images of type torch.Tensor
@@ -119,7 +119,23 @@ def evaluate_batch(model:torch.nn.Module, image_batch:torch.Tensor, targets:list
     """
 
     metric = MeanAveragePrecision(iou_type = 'bbox', class_metrics = True)
-    metric = update_metric_on_batch(metric, model, image_batch, targets)
+    update_metric_on_batch(metric, model, image_batch, targets)
     mAP_dict = metric.compute()
     return mAP_dict
     
+
+if __name__ == "__main__":
+    
+    from create_model import get_model_fasterrcnn
+
+    model = get_model_fasterrcnn(num_classes=3)
+    image_batch = torch.rand(2,3,224,224)
+
+    targets = {
+        'boxes':torch.tensor([[0,0,100,100],[100,100,200,200]]),
+        'labels':torch.tensor([0,1])
+        }
+
+    mAP_dict = evaluate_batch(model,image_batch,[targets,targets])
+
+    print(mAP_dict)
